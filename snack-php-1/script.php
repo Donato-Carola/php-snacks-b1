8393a2b7e94ac3b4efaafe3e563fbb775c9aef00
Donato-Carola <don94@live.it>
2024-01-15 12:25:52 +0100

    array

---
+++ snack-php-1/script.php
@@ -0,0 +1,40 @@
+<?php
+/*Creiamo un array contenente le partite di basket di un'ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite,
+ punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema:
+
+Olimpia Milano - Cantù | 55-60*/
+
+
+
+//creiamo l'array
+//
+
+
+
+
+$partite = [
+    [
+        'squadra1' => 'milano',
+        'squadra2' =>'Cantù',
+        'punteggio1' => 55,
+        'punteggio2' => 60
+    ],
+    [
+        'squadra1' => 'napoli',
+        'squadra2' =>'roma',
+        'punteggio1' => 40,
+        'punteggio2' => 22
+    ],
+
+
+]
+
+
+
+
+
+
+
+
+
+?>
