ba3dd80b9273fceb3696b827af57d52837adf0ff
Donato-Carola <don94@live.it>
2024-01-16 12:29:59 +0100

    form per chiedere i due numeri

---
+++ snack 2.5/index.php
@@ -0,0 +1,33 @@
+<?php    //espongo la media di due valori inseriti dall'utente con chiamata GET
+   
+    
+   
+
+
+
+?>
+
+
+
+
+
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>media</title>
+</head>
+<body>
+    
+  <form action="./index.php" method="get">
+    <label for="numberOne">inserisci primo numero</label>
+     <input type="number" name="numberOne" id="numberOne">
+
+     <label for="numberTwo">inserisci secondo numero </label>
+     <input type="number" name="numberTwo" id="numberTwo">
+
+  </form>
+
+</body>
+</html>
