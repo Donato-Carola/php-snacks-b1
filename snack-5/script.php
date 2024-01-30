f4fce12e84e9414cbced2809571c6ded05133c3e
Donato-Carola <don94@live.it>
2024-01-30 09:43:59 +0100

    oggetto e construct

---
+++ snack-5/script.php
@@ -0,0 +1,27 @@
+<?php 
+
+
+class Persona {
+    public $name;
+    public $surname;
+    public $age;
+
+
+
+    public function __construct($_name, $_surname, $_age)
+    {
+        $this->name = $_name;
+        $this->surname = $_surname;
+        $this->age = $_age;
+        
+    }
+
+}
+
+
+
+
+
+
+
+?>
