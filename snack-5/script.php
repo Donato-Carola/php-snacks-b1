5c34430343cabb296d023265a20342f5cf74ec56
Donato-Carola <don94@live.it>
2024-01-30 10:13:57 +0100

    esercizio snack 5

--- snack-5/script.php
+++ snack-5/script.php
@@ -1,59 +1,38 @@
-<?php 
+<?php
 
 
-class Persona {
+class Persona
+{
     public $name;
     public $surname;
     public $age;
 
 
 
-    public function __construct(string $_name,string $_surname,int $_age)
-    {
-        $this->name = $_name;
-        $this->surname = $_surname;
-        $this->age = $_age;
-        
-    }
 
-    public function setAge($_age) {
+    public function setAge($_age)
+    {
         if (!is_int($_age)) {
-            throw new InvalidArgumentException('L\'età deve essere un numero intero.');
+            throw new Exception('L\'età deve essere un numero intero.');
+        } else {
+
+
+            $this->age = $_age;
         }
-
-        $this->age = $_age;
     }
-
-    public function getAge()
-    {
-        return $this->age;
-    }
-
-
-
 }
 
+
+$persona = new Persona();
 try {
-    $persona = new Persona("Mario", "Rossi", 30);
-    
 
-    
-    $persona->setAge(35);
-    echo "Nuova età: " . $persona->getAge() . "<br>";
 
-    
-    $persona->setAge("non un numero"); 
 
-} catch (InvalidArgumentException $e) {
+
+
+
+
+    $persona->setAge("non un numero");
+} catch (Exception $e) {
     echo "Errore: " . $e->getMessage();
 }
-
-
-
-
-
-
-
-
-
-?>
